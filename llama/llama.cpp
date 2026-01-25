831c46e195feecd867535404a18c911e04d32929
KevinX <Xzb04720638k>
2026-01-25 18:03:17 -0500

    update

--- llama/llama.cpp
+++ llama/llama.cpp
@@ -1,1 +1,11 @@
 llama-server -hf unsloth/Qwen3-4B-Thinking-2507-GGUF --jinja -c 0 --port 8033
+
+
+llama-server -hf Llama-3.2-3B-Instruct-Q8_0.gguf --jinja -c 0 --port 8033
+
+
+
+https://blog.csdn.net/2401_85390073/article/details/157362386?spm=1001.2014.3001.5502
+curl -L -O https://huggingface.co/mradermacher/Hunyuan-4B-Instruct-GGUF/resolve/main/Hunyuan-4B-Instruct.Q4_K_M.gguf
+
+llama-server -m Hunyuan-4B-Instruct.Q4_K_M.gguf --jinja -c 0 --port 8033
