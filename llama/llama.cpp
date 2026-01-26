f4dfdc3285e086a4451069cc883fa1f1a2a6db0d
KevinX <Xzb04720638k>
2026-01-25 19:38:44 -0500

    update

--- llama/llama.cpp
+++ llama/llama.cpp
@@ -9,3 +9,6 @@ https://blog.csdn.net/2401_85390073/article/details/157362386?spm=1001.2014.3001
 curl -L -O https://huggingface.co/mradermacher/Hunyuan-4B-Instruct-GGUF/resolve/main/Hunyuan-4B-Instruct.Q4_K_M.gguf
 
 llama-server -m Hunyuan-4B-Instruct.Q4_K_M.gguf --jinja -c 0 --port 8033
+
+https://huggingface.co/Qwen/Qwen1.5-0.5B-Chat/tree/main
+pip install -r requirements.txt
