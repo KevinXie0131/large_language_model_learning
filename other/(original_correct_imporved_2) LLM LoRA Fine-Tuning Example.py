8e96c3a25123f54f5cec6bdc6299400dc99ef930
KevinX <Xzb04720638k>
2026-02-12 16:41:26 -0500

    update

--- other/(original_correct_imporved_2) LLM LoRA Fine-Tuning Example.py
+++ other/(original_correct_imporved_2) LLM LoRA Fine-Tuning Example.py
@@ -4,6 +4,7 @@
 # -------------------------------------------------------
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+# Qwen/Qwen2.5-3B-Instruct works too
 tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0")
 model = AutoModelForCausalLM.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0", device_map="auto")
 
